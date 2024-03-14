f0de1ad8f04d9a92380e78536f891d27ec111346
Avila-Pato <152195893+Avila-Pato@users.noreply.github.com>
2024-03-13 21:04:23 -0300

    React-Fetch

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+import { Suspense } from 'react';
+import  { fetchData } from './fetchData.js'
+import './App.css'
+
+// eslint-disable-next-line no-unused-vars
+const apiData =fetchData ("https://jsonplaceholder.typicode.com/users")
+function App() {
+  const data = apiData.read();
+  return (
+    <div className="App">
+    <h1> Ejemplo de Fetch</h1>
+    <Suspense fallback={<h1>Loading...</h1>}>
+    <ul className="card"> 
+    {data?.map((user) => (
+      <li key=
+      {user.id}>
+      {user.name}</li>
+    ))}
+    </ul>
+    </Suspense>
+  </div>
+  );
+}
+
+export default App
+
+
+
+
+
+
+
+// import { useFetch } from './assets/useFetch';
+// function App() {
+//   const { data, loading, error, handleCancelRequest } = useFetch("https://jsonplaceholder.typicode.com/users")
+  
+
+//   return (
+//          <div className="App">
+//           <h1>Fetch like a Pro</h1>
+    
+//           <button onClick={handleCancelRequest}>Cancel Request</button>
+//            <ul className="card">
+//              {error && <li>Error: {error}</li>}
+//              {loading && <li>Loading...</li>}
+//              {data?.map((item) => (
+//                <li key={item.id}>{item.title}</li>
+//              ))}
+//            </ul>
+//          </div>
+//       );
+//     }
+   
+// export default App
---
+++ src/assets/useFetch.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(url) { 
+  const [data, setData] = useState(null);   
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [controller, setController] = useState(null);
+  
+  useEffect(() => {
+    const abortController = new AbortController();
+    setController(abortController);
+    setLoading(true);
+
+    fetch(url, { signal: abortController.signal })
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        if (error.name === "AbortError") {
+          console.log("Cancelled request");
+        } else {
+          setError(error);
+        }
+      })
+      .finally(() => setLoading(false));
+
+    return () => abortController.abort();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleCancelRequest = () => {
+    if (controller) {
+      controller.abort();
+      setError("Cancelled Request");
+    }
+  };
+
+  return { data, loading, error, handleCancelRequest };
+}
